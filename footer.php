41136e18fec000eafb358a2ee526d204fe22a10a
gustavo <gustavodevaler@gmail.com>
2020-01-29 23:33:36 -0300

    .

--- footer.php
+++ footer.php
@@ -3,7 +3,25 @@
 	.footer{
 		clear: both;
 	}
+	.content{
+		width: 100%;
+		min-height: 80px;
+	}
 </style>
 <section class="footer">
-	footer
+	<div class="content">
+
+		<ul>
+			<li><a href="">Inicio</a></li>
+			<li><a href="">Pedidos</a></li>
+			<li><a href="">Ventas</a></li>
+			<li><a href="">Stock</a></li>
+			<li><a href="">Clientes</a></li>
+		</ul>
+
+		<div class="copyr">
+			Copyrigth 2020 Gustavo Valerio
+		</div>
+		
+	</div>
 </section>
